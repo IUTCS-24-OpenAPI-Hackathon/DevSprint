0efb468c3cbe4a0f3aa2c5b4e66b00ba2addf968
Azizur Rahaman <91342806+azizur-rahaman@users.noreply.github.com>
2024-04-26 15:56:10 +0600

    serach bar added

--- app/devsprint/lib/app/aurora.dart
+++ app/devsprint/lib/app/aurora.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '/services/l10n/locale_provider.dart';
 import '/services/router.dart';
@@ -14,15 +15,23 @@ class AuroraApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final themeData = ref.watch(themesProvider);
 
-    return MaterialApp.router(
-      routerConfig: ref.watch(goRouterProvider),
-      theme: themeData.$1,
-      darkTheme: themeData.$2,
-      themeMode: ref.watch(themeSettingProvider),
-      locale: ref.watch(l10nProvider),
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // Use builder only if you need to use library outside ScreenUtilInit context
+      builder: (_, child) {
+        return MaterialApp.router(
+          routerConfig: ref.watch(goRouterProvider),
+          theme: themeData.$1,
+          darkTheme: themeData.$2,
+          themeMode: ref.watch(themeSettingProvider),
+          locale: ref.watch(l10nProvider),
+          debugShowCheckedModeBanner: false,
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+        );
+      },
     );
   }
 }
--- app/devsprint/lib/src/ui/screens/home/home_screen.dart
+++ app/devsprint/lib/src/ui/screens/home/home_screen.dart
@@ -1,51 +1,114 @@
+import 'dart:ffi';
+
 import 'package:devsprint/src/services/location/location_repo.dart';
 import 'package:devsprint/src/services/location/models/locatin_state_model.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:material_floating_search_bar_2/material_floating_search_bar_2.dart';
 
-import '/services/l10n/locale_extention.dart';
-import '/services/router.dart';
-import '/src/global/global.dart';
-import '/src/ui/widgets/icons.dart';
-import 'home_screen_providers.dart';
+const bgColor = Color(0xff054167);
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final count = ref.watch(countProvider);
-    final colors = ref.watch(countColorsProvider);
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  LocationState? _locationState;
+
+  final _textlocationController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _getLocation();
+  }
+
+  _getLocation() async {
+    LocationState lState = await LocationRepository().getLocation();
+
+    if (lState.position == null) {
+      // Fluttertoast.showToast(
+      //     msg: "Location is not granted, Please Click again");
+    } else {
+      setState(() {
+        _locationState = lState;
+      });
+    }
+  }
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(context.l10n.appName),
-        actions: [
-          IconButton(
-            icon: const IIcon(
-              FontAwesomeIcons.gear,
-              fontAwesome: true,
-            ),
-            onPressed: () {
-              context.pushNamed(AppRoutes.settings.name);
-            },
+   
+      body: Scaffold(
+        body: SafeArea(
+            child: Container(
+          color: bgColor,
+          width: double.maxFinite,
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              buildFloatingSearchBar(),
+            ],
           ),
-        ],
+        )),
       ),
-        body: Center(
-        child: ElevatedButton(
-          onPressed: () async{
-            LocationState _locationState = await  LocationRepository().getLocation();
-
-            debugPrint(_locationState.toString());
-
-          },
-          child: Text("Get Location"),)
-        
-         ),
     );
   }
+
+Widget buildFloatingSearchBar() {
+  final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+
+  return FloatingSearchBar(
+    hint: 'Search...',
+    scrollPadding: EdgeInsets.only(top: 16.h, bottom: 56.h),
+    transitionDuration: const Duration(milliseconds: 800),
+    transitionCurve: Curves.easeInOut,
+    physics: const BouncingScrollPhysics(),
+    axisAlignment: isPortrait ? 0.0 : -1.0,
+    openAxisAlignment: 0.0,
+    width: isPortrait ? 600.w : 500.w,
+    debounceDelay: const Duration(milliseconds: 500),
+    onQueryChanged: (query) {
+      // Call your model, bloc, controller here.
+    },
+    // Specify a custom transition to be used for
+    // animating between opened and closed stated.
+    transition: CircularFloatingSearchBarTransition(),
+    actions: [
+      FloatingSearchBarAction(
+        showIfOpened: false,
+        child: CircularButton(
+          icon: const Icon(Icons.place),
+          onPressed: () {},
+        ),
+      ),
+      FloatingSearchBarAction.searchToClear(
+        showIfClosed: false,
+      ),
+    ],
+    builder: (context, transition) {
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Material(
+          color: Colors.white,
+          elevation: 4.0,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: []
+          ),
+        ),
+      );
+    },
+  );
+}
+
+
 }
